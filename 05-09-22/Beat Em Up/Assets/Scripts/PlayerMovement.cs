146d8bdde195f9b60a36ef98239e099c3e708694
Hassanali4 <89867379+Hassanali4@users.noreply.github.com>
2023-01-13 18:15:27 +0500

    First form of old Project file Uncomplete
    Description: This is an incomplete form of the project just the initial draft of the project that I have created so that I could comeback and practice the concepts again if wanted to.

---
+++ 05-09-22/Beat Em Up/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Private Player Animatoin Player_Animation;
+    private Rigidbody myBody;
+
+    public float walk_Speed = 3f;
+    public float z_speed = 1.5f;
+
+    private float rotation_Y = -90f;
+    private float rotation_Speed = 15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
